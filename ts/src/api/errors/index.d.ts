401a3b045dd7504451c76f3ce5de741a48cf9c27
glaliberte05 <109228515+glaliberte05@users.noreply.github.com>
2024-12-19 11:06:13 -0500

    TypeScript SDK implementation (#23)
    Update to latest, add readme

---
+++ ts/src/api/errors/index.d.ts
@@ -0,0 +1,7 @@
+export * from "./BadRequestError";
+export * from "./UnauthorizedError";
+export * from "./ForbiddenError";
+export * from "./TooManyRequestsError";
+export * from "./ConflictError";
+export * from "./NotFoundError";
+export * from "./UnprocessableEntityError";
---
+++ ts/src/serialization/resources/executeTransaction/client/requests/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TransactionUpdate = exports.ExecuteMintTransaction = exports.ExecuteTradeTransaction = exports.ExecuteTransferTransaction = exports.ExecutePaymentTransaction = exports.ExecuteWithdrawalTransaction = exports.ExecuteDepositTransaction = void 0;
+var ExecuteDepositTransaction_1 = require("./ExecuteDepositTransaction");
+Object.defineProperty(exports, "ExecuteDepositTransaction", { enumerable: true, get: function () { return ExecuteDepositTransaction_1.ExecuteDepositTransaction; } });
+var ExecuteWithdrawalTransaction_1 = require("./ExecuteWithdrawalTransaction");
+Object.defineProperty(exports, "ExecuteWithdrawalTransaction", { enumerable: true, get: function () { return ExecuteWithdrawalTransaction_1.ExecuteWithdrawalTransaction; } });
+var ExecutePaymentTransaction_1 = require("./ExecutePaymentTransaction");
+Object.defineProperty(exports, "ExecutePaymentTransaction", { enumerable: true, get: function () { return ExecutePaymentTransaction_1.ExecutePaymentTransaction; } });
+var ExecuteTransferTransaction_1 = require("./ExecuteTransferTransaction");
+Object.defineProperty(exports, "ExecuteTransferTransaction", { enumerable: true, get: function () { return ExecuteTransferTransaction_1.ExecuteTransferTransaction; } });
+var ExecuteTradeTransaction_1 = require("./ExecuteTradeTransaction");
+Object.defineProperty(exports, "ExecuteTradeTransaction", { enumerable: true, get: function () { return ExecuteTradeTransaction_1.ExecuteTradeTransaction; } });
+var ExecuteMintTransaction_1 = require("./ExecuteMintTransaction");
+Object.defineProperty(exports, "ExecuteMintTransaction", { enumerable: true, get: function () { return ExecuteMintTransaction_1.ExecuteMintTransaction; } });
+var TransactionUpdate_1 = require("./TransactionUpdate");
+Object.defineProperty(exports, "TransactionUpdate", { enumerable: true, get: function () { return TransactionUpdate_1.TransactionUpdate; } });
